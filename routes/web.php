b9ebb0fa5236801f81434be91badfd15daae6dd9
Imon <contact@imonweb.co.uk>
2025-12-03 23:17:34 +0000

    artisan list

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/about', 'about')->name('about');
+
+Route::fallback(function() {
+    return 'fallback';
+});
+       
